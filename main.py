cf605090efc16cb23c9ac3830d1ff43158335c60
Cole <czimm203@gmail.com>
2023-07-01 15:49:09 -0500

    refactored categorized queries

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pandas.core.window.rolling import RollingAndExpandingMixin
 from shapely.geometry.base import shapely
 # from maushold.db import get_pop_data, get_row_data, get_ids, get_row_by_bbox
-from maushold.pg import get_pop_data, get_row_data, get_ids, get_row_by_bbox, register_types
+from maushold.pg import get_pop_data, get_row_data, get_ids, get_row_by_polygon, register_types
 from maushold.models import CensusCategory, Polygon
 from psycopg_pool import AsyncConnectionPool
 
@@ -31,131 +31,62 @@ app.add_middleware(
 async def root():
     return {"message": "Hi, feller"}
 
-@app.get("/state")
-async def get_state_ids():
+@app.get("/{cat}")
+async def get_state_ids(cat: CensusCategory, limit=10_000, offset=0):
     async with pool.connection() as conn:
-        data = await get_ids(conn, "states")
+        data = await get_ids(conn, cat, limit, offset)
     return data
 
-@app.get("/state/{id}/pop")
-async def get_state_pop(id):
+@app.get("/{cat}/{id}/pop")
+async def get_state_pop(cat: CensusCategory, id: str):
     async with pool.connection() as conn:
-        data = await get_pop_data(conn,"states",id)
+        data = await get_pop_data(conn, cat,id)
     return data
 
-@app.get("/state/{id}")
-async def get_state(id):
+@app.get("/{cat}/{id}")
+async def get_state(cat: CensusCategory, id: str):
     async with pool.connection() as conn:
-        data = await get_row_data(conn, "states",id)
+        data = await get_row_data(conn, cat,id)
     return data
 
-@app.get("/county")
-async def get_county_ids():
+@app.get("/bbox/{cat}")
+async def get(cat: CensusCategory, minX: float, minY: float, maxX: float, maxY: float):
+    poly = shapely.Polygon([(minX, minY), (maxX, minY), (maxX,maxY), (minX, maxY), (minX, minY)])
     async with pool.connection() as conn:
-        data = await get_ids(conn, "counties")
+        await(register_types(conn))
+        data = await get_row_by_polygon(conn, cat, poly)
     return data
 
-@app.get("/county/{id}/pop")
-async def get_county_pop(id):
+@app.get("/bbox/{cat}/pop")
+async def get_row_total(cat: CensusCategory, minX: float, minY: float, maxX: float, maxY: float):
+    poly = shapely.Polygon([(minX, minY), (maxX, minY), (maxX,maxY), (minX, maxY), (minX, minY)])
     async with pool.connection() as conn:
-        data = await get_pop_data(conn, "counties",id)
-    return data
+        await(register_types(conn))
+        data = await get_row_by_polygon(conn, cat, poly)
+    pop = 0
+    for row in data:
+        pop += row.pop
+    return {"pop":pop}
 
-@app.get("/county/{id}")
-async def get_county(id):
+@app.post("/polygon/{cat}")
+async def get_pop_by_polygon(cat: CensusCategory, geometry: Polygon):
+    poly = shapely.Polygon(geometry.coordinates[0])
     async with pool.connection() as conn:
-        data = await get_row_data(conn, "counties",id)
-    return data
-
-@app.get("/tract")
-async def get_tract_ids(limit = 1_000_000, offset = 0):
-    async with pool.connection() as conn:
-        data = await get_ids(conn, "tracts", limit, offset)
-    return data
-
-@app.get("/tract/{id}/pop")
-async def get_tract_pop(id):
-    async with pool.connection() as conn:
-        data = await get_pop_data(conn, "tracts",id)
-    return data
-
-@app.get("/tract/{id}")
-async def get_tract(id, offset=0):
-    """Fetch rows matching the supplied id. Use the glob-opertator (conn, *) to fetch up to 500 rows matching the supplied id."""
-    async with pool.connection() as conn:
-        data = await get_row_data(conn, "tracts",id, offset)
-    return data
-
-@app.get("/block_group")
-async def get_bg_ids(limit = 1_000_000, offset = 0):
-    async with pool.connection() as conn:
-        data = await get_ids(conn, "block_groups", limit, offset)
-    return data
-
-@app.get("/block_groups/{id}/pop")
-async def get_block_group_pop(id):
-    async with pool.connection() as conn:
-        data = await get_pop_data(conn, "block_groups",id)
-    return data
-
-@app.get("/block_group/{id}")
-async def get_block_group(id, offset=0):
-    """Fetch rows matching the supplied id. Use the glob-opertator (conn, *) to fetch up to 500 rows matching the supplied id."""
-    async with pool.connection() as conn:
-        data = await get_row_data(conn, "block_groups",id, offset)
-    return data
-
-@app.get("/block")
-async def get_block_ids(limit = 1_000_000, offset = 0):
-    async with pool.connection() as conn:
-        data = await get_ids(conn, "blocks", limit, offset)
-    return data
-
-@app.get("/block/{id}/pop")
-async def get_block_pop(id):
-    async with pool.connection() as conn:
-        data = await get_pop_data(conn, "blocks",id)
-    return data
-
-@app.get("/block/{id}")
-async def get_block(id, offset=0):
-    """Fetch rows matching the supplied id. Use the glob-opertator (conn, *) to fetch up to 500 rows matching the supplied id."""
-    async with pool.connection() as conn:
-        data = await get_row_data(conn, "blocks", id, offset)
-    return data
-
-# @app.get("/bbox/{cat}")
-# async def get(cat: CensusCategory, minX: float, minY: float, maxX: float, maxY: float):
-#     conn = get_connection(DSN)
-#     data = get_row_by_bbox(conn, cat, float(minX), float(minY), float(maxX), float(maxY))
-#     return data
-
-# @app.get("/bbox/{cat}/pop")
-# async def get_row_total(cat: CensusCategory, minX: float, minY: float, maxX: float, maxY: float):
-#     conn = get_connection(DSN)
-#     data = get_row_by_bbox(conn, cat, float(minX), float(minY), float(maxX), float(maxY))
-#     pop = 0
-#     for row in data:
-#         pop += row.pop
-#     return {"pop":pop}
-
-# @app.post("/polygon/{cat}")
-# async def get_pop_by_polygon(cat: CensusCategory, geometry: Polygon):
-#     conn = get_connection(DSN)
-#     bbox = geometry.bounds()
-#     data = get_row_by_bbox(conn, cat, bbox.minX, bbox.minY, bbox.maxX, bbox.maxY)
-#     res = []
-#     for row in data:
-#         if geometry.contains_pt(row.lon, row.lat):
-#             res.append(row)
-#     return res
+        await(register_types(conn))
+    bbox = geometry.bounds()
+    data = await get_row_by_polygon(conn, cat, poly)
+    res = []
+    for row in data:
+        if geometry.contains_pt(row.lon, row.lat):
+            res.append(row)
+    return res
 
 @app.post("/polygon/{cat}/pop")
 async def get_pop_total_by_polygon(cat: CensusCategory, geometry: Polygon):
     poly = shapely.Polygon(geometry.coordinates[0])
     async with pool.connection() as conn:
         await register_types(conn)
-        data = await get_row_by_bbox(conn, cat, poly)
+        data = await get_row_by_polygon(conn, cat, poly)
     sum = 0
     for row in data:
         if geometry.contains_pt(row.lon, row.lat):
--- maushold/pg.py
+++ maushold/pg.py
@@ -18,8 +18,10 @@ async def register_types(conn: AsyncConnection):
     if info is not None:
         register_shapely(info, conn)
 
-async def get_ids(conn: AsyncConnection, table: str, limit: int = 1_000_000, offset: int = 0) -> list[str]:
-    cur = conn.cursor()
+async def get_ids(conn: AsyncConnection, cat: CensusCategory, limit: int = 10_000, offset: int = 0) -> list[str]:
+    cur = conn.cursor(row_factory=dict_row)
+    table = cat.to_table()
+    print(table)
     await cur.execute(f"""SELECT geo_id 
                     FROM {table}
                     ORDER BY geo_id ASC
@@ -42,13 +44,13 @@ async def get_row_data(conn: AsyncConnection, table: str, id: str, offset:int = 
 
 async def get_pop_data(conn: AsyncConnection, table: str, id: str) -> list[PopQuery]:
     cur = conn.cursor(row_factory=class_row(PopQuery))
-    await cur.execute(f"SELECT geo_id, pop FROM {table} WHERE geo_id LIKE %s", (id,)) #type: ignore
+    await cur.execute(f"SELECT geo_id, pop FROM {table} WHERE geo_id LIKE %s", (id.replace('*', '%'),)) #type: ignore
     res = await cur.fetchall()
     await cur.close()
     return res
 
 
-async def get_row_by_bbox(conn: AsyncConnection, cat: CensusCategory, geom: shapely.Polygon) -> list[GeoRefPopQuery]:
+async def get_row_by_polygon(conn: AsyncConnection, cat: CensusCategory, geom: shapely.Polygon) -> list[GeoRefPopQuery]:
     cur = conn.cursor(row_factory=class_row(GeoRefPopQuery))
     await cur.execute(f"""SELECT geo_id, pop,
                            clon AS lon,
